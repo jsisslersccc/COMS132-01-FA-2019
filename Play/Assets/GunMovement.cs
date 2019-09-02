fd81d3ab38d5e6ee7931dfae1a43a792f3dafa50
John Sissler <jsissler@gmail.com>
2019-09-02 18:23:43 -0400

    idea for micro game project

---
+++ Play/Assets/GunMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMovement : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 5f;
+
+    void Update()
+    {
+        float rotation = Input.GetAxis("Horizontal");
+        transform.Rotate(0, 0, rotation * rotationSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
