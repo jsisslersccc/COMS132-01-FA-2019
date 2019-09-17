5526aa80bb2549e77d2593c89bb6d2d0c46f20f1
jsissler <jsissler@gmail.com>
2019-09-17 15:56:18 -0400

    add

---
+++ Enemy Class Sample Project/Assets/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    public float fireRate = 0.3f;
+
+    void Update()
+    {
+        Move();
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        GameObject other = coll.gameObject;
+        switch(other.tag)
+        {
+            case "Hero":
+                break;
+            case "HeroLaser":
+                Destroy(this.gameObject);
+                break;
+        }
+    }
+
+    public Vector2 pos
+    {
+        get
+        {
+            return this.transform.position;
+        }
+
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+}
