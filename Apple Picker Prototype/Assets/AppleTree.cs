3f4af1d25df388d91c8572f10aaa5128cc7db86d
jsissler <jsissler@gmail.com>
2019-09-24 15:52:43 -0400

    copied from personal acct

---
+++ Apple Picker Prototype/Assets/AppleTree.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change directions
+    public float chanceToChangeDirections = 0.1f;
+
+    // Rate at which Apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Dropping apples every second
+        Invoke("DropApple", 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Changing Directions
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Changing Directions Randomly is now time-based because of FixedUpdate()
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+}
