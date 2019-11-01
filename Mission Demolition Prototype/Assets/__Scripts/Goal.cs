3b49ec8ba22101f12516f4742e63ae7b0c1f4203
John Sissler <jsissler@gmail.com>
2019-11-01 16:01:48 -0400

    Added explosion afx and vfx when projectile hits goal

--- Mission Demolition Prototype/Assets/__Scripts/Goal.cs
+++ Mission Demolition Prototype/Assets/__Scripts/Goal.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class Goal : MonoBehaviour {
     public static bool goalMet = false;
 
+    public GameObject explosionVFX;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Projectile")
@@ -13,6 +15,16 @@ public class Goal : MonoBehaviour {
             Color c = mat.color;
             c.a = 1;
             mat.color = c;
+
+            if (explosionVFX)
+            {
+                GameObject vfx = Instantiate(explosionVFX);
+                vfx.transform.position = other.gameObject.transform.position;
+            }
+
+            AudioSource afx = GetComponent<AudioSource>();
+            if (afx)
+                afx.Play();
         }
     }
 }
